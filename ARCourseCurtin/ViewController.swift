b6c050e3444e3bf463a351bc15392ae6fd21166f
camilo cabana <camilo_11_04@hotmail.com>
2020-08-11 20:09:18 +0900

    remote

--- ARCourseCurtin/ViewController.swift
+++ ARCourseCurtin/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //
         // Set the view's delegate
         sceneView.delegate = self
         
